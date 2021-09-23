8ff27edbc756d952b09763cb8188229ffd34ee02
len <lenyu@xiaoe-tech.com>
2021-09-23 18:12:30 +0800

    init

---
+++ helloword.go
@@ -0,0 +1,13 @@
+/**
+* @Author: len
+* @Description:
+* @File: helloword
+* @Date: 2021/9/23 18:11
+ */
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("helloworld")
+}
--- readme.md
+++
@@ -1,3 +0,0 @@
-## go语言圣经 代码库
-
-[go语言圣经中文版](https://books.studygolang.com/gopl-zh/)
